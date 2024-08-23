ca89a5297eee3fda6f4885d6b6db78b4e67a46ac
YuriVini <yuri.ybgt.yfo@gmail.com>
2024-08-23 13:15:03 -0300

    feat: backend creation and configs

---
+++ back/api/api.go
@@ -0,0 +1,49 @@
+package api
+
+import (
+	"back/store/pgstore"
+	"net/http"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+)
+
+type apiHandler struct {
+	q *pgstore.Queries
+	r *chi.Mux
+}
+
+func (h apiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.r.ServeHTTP(w, r)
+}
+
+func NewHandler(q *pgstore.Queries) http.Handler {
+	a := apiHandler{
+		q: q,
+	}
+
+	r := chi.NewMux()
+
+	r.Use(middleware.RequestID, middleware.Recoverer, middleware.Logger)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	r.Route("/api", func(r chi.Router) {
+		r.Route("/user", func(r chi.Router) {
+			r.Post("/", a.createUser)
+		})
+	})
+
+	return r
+}
+
+func (h apiHandler) createUser(w http.ResponseWriter, r *http.Request) {
+	return
+}
