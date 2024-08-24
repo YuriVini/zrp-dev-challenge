d9f6f9aa5400834d2b3a0396cb634e8adb73bfd5
YuriVini <yuri.ybgt.yfo@gmail.com>
2024-08-24 08:04:15 -0300

    feat: threats state

---
+++ web/src/zustand-states/useThreats.ts
@@ -0,0 +1,28 @@
+import { create } from 'zustand'
+
+type DangerLevel = "God" | "Dragon" | "Tiger" | "Wolf"
+
+interface Threat {
+  location: [{
+    lat: number
+    lng: number
+  }]
+  dangerLevel: DangerLevel
+  monsterName: string
+  monster: {
+    name: string
+    description: string
+    url: string
+  }
+}
+
+interface Store {
+  threats: Threat[];
+  addThreat: (newThreat: Threat) => void
+}
+
+export const useThreats = create<Store>()((set) => ({
+  threats: [],
+  addThreat: (newThreat: Threat) => set((_state) => ({ threats: [..._state.threats, newThreat] })),
+}))
+
