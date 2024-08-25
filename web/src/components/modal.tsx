fe1af22f0f22daec214be849296b178d3faa4053
YuriVini <yuri.ybgt.yfo@gmail.com>
2024-08-25 14:49:39 -0300

    fix: close button position after add NavBar

--- web/src/components/modal.tsx
+++ web/src/components/modal.tsx
@@ -8,8 +8,8 @@ export const Modal = ({ visible, onClose, children }: PropsWithChildren<ModalPro
   if (!visible) return null;
 
   return (
-    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 outline-none focus:outline-none bg-zinc-950 bg-opacity-50">
-      <button className="p-2 absolute top-4 right-10 text-4xl" onClick={onClose}>
+    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto z-10 fixed inset-0 outline-none focus:outline-none bg-zinc-950 bg-opacity-50">
+      <button className="p-2 absolute top-16 right-10 z-20 text-4xl" onClick={onClose}>
         X
       </button>
       {children}
